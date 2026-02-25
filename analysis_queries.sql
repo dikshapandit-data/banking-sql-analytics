b864a6304e67d7b36a107db1af7ea1de3463da57
dikshapandit-data <dikshapandit5669@gmail.com>
2026-02-25 23:41:11 +0530

    Update analysis_queries.sql

--- analysis_queries.sql
+++ analysis_queries.sql
@@ -1,19 +1,17 @@
 -- 1. Total transaction amount per customer
 SELECT c.name, SUM(t.amount) AS total_transaction_amount 
-FROM customers c JOIN transaction t ON c.customer_id = t.customer_id
+FROM customers c JOIN transactions t ON c.customer_id = t.customer_id
 GROUP BY c.name;
 
 -- 2. Identify high-value customers (Total > 10000)
 SELECT c.name, SUM(t.amount) AS total_transaction_amount 
-FROM customers c JOIN transaction t ON c.customer_id = t.customer_id
+FROM customers c JOIN transactions t ON c.customer_id = t.customer_id
 GROUP BY c.name
 HAVING SUM(t.amount) > 10000;
 
 -- 3. Monthly transaction trend
-SELECT * FROM
-(SELECT DATE_FORMAT(transaction_date, '%Y-%m') AS month, SUM(amount) AS monthly_total
-FROM transactions) a
-GROUP BY monthly_total;
+SELECT DATE_FORMAT(transaction_date, '%Y-%m') AS month, SUM(amount) AS monthly_total
+FROM transactions GROUP BY month;
 
 -- 4. Customers with no transactions
 SELECT c.name
@@ -21,15 +19,15 @@ FROM customers c LEFT JOIN transactions t ON c.customer_id=t.customer_id
 WHERE t.transaction_id IS NULL;
 
 -- 5. Rank customers by total transaction amount
-SELECT c.name, SUM(t.amount) AS total_transaction_amount, RANK() OVER (ORDER BY SUM(t.amount) DESC) AS rank
-FROM customers c JOIN transaction t ON c.customer_id = t.customer_id
+SELECT c.name, SUM(t.amount) AS total_transaction_amount, DENSE_RANK() OVER (ORDER BY SUM(t.amount) DESC) AS drnk
+FROM customers c JOIN transactions t ON c.customer_id = t.customer_id
 GROUP BY c.name;
 
 -- 6. Top 2 customers by revenue
 SELECT * FROM (
 SELECT c.name, SUM(t.amount) AS total_transaction_amount, ROW_NUMBER() OVER (ORDER BY SUM(t.amount) DESC) AS row_num
-FROM customers c JOIN transaction t ON c.customer_id = t.customer_id
-GROUP BY c.name) a
+FROM customers c JOIN transactions t ON c.customer_id = t.customer_id
+GROUP BY c.name)a
 WHERE row_num <=2 ;
 
 -- 7. Running total of transactions by date
@@ -41,7 +39,7 @@ ORDER BY transaction_date;
 -- 8. Customer segmentation based on total spending
 SELECT c.name, SUM(t.amount) AS total_spent,
 CASE WHEN SUM(t.amount) >= 15000 THEN 'Premium'
-     WHEN SUM(t.amount) BETWEEN 8000 AND 14999 THEN 'Gold'
+     WHEN SUM(t.amount) BETWEEN 6000 AND 14999 THEN 'Gold'
      ELSE 'Standard'
 END AS customer_segment
 FROM customers c JOIN transactions t ON c.customer_id = t.customer_id
